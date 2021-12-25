f95dd1c231c140e7a7c375da492eb58cca0fbbb0
Sultan <m.sultan338@gmail.com>
2021-12-25 16:50:55 +0500

    OSSU

---
+++ Intro to CS/1/Getting started.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 25 15:25:07 2021
+
+@author: sultan
+"""
+print(6 + 4 * 10)
+print((6 + 4) * 10)
+print(23.0*5)
+
+from math import sqrt
+
+a= 34
+b= 68
+c= 510
+d= b*b - 4*a*c
+print(d)
+#formula = (-b +sqrt(b*b - 4*a*c))/(2*a)
+
+#print(formula)
+
+import math
+print(math.cos(3.4)**2+math.sin(3.4)**2)
+
