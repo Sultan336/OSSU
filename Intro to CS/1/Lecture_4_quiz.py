df6928ca92a323606f0231bd4e6d36cc32932806
Sultan <m.sultan338@gmail.com>
2022-01-02 16:50:59 +0500

    update

---
+++ Intro to CS/1/Lecture_4_quiz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  2 15:55:29 2022
+
+@author: sultan
+"""
+
+def add(x, y):
+    return x+y
+
+def mult(x, y):
+    print(x*y)
+
+add(1,2)
+print(add(2,3))
+mult(3,4)
+print(mult(4,5))
