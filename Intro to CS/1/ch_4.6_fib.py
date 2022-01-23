967831f7c15f4cc0ba8bf0a940c40741e8720b31
Sultan <m.sultan338@gmail.com>
2022-01-24 00:13:52 +0500

    Update ch_4.6_fib.py

--- Intro to CS/1/ch_4.6_fib.py
+++ Intro to CS/1/ch_4.6_fib.py
@@ -17,3 +17,34 @@ def testFib(n):
     for i in range(n+1):
         print('fib of', i, '=', fib(i))
 testFib(2)
+
+def isPalindrome(s):
+    """ Assumes s is str
+        Returns True if letters in s form a palindrome;
+        False otherwise. 
+        Non-letters and capitalization are ignored"""
+    def toChars(s):
+        s = s.lower()
+        letters = ''
+        for c in s:
+            if c in 'abcdefghijklmnopqrstuvwxyz':
+                letters += c
+        return letters
+    def isPal(s):
+        if len(s) <= 1:
+            return True
+        else:
+            answer = s[0] == s[-1] and isPal(s[1:-1])
+            print(f'About to return {answer} for {s} ')
+            return answer
+    return isPal(toChars(s))
+    
+def testIsPalindrome():
+    print('Try dogGod')
+    print(isPalindrome('dogGod'))
+    print('Try doGood')
+    print(isPalindrome('doGood'))
+testIsPalindrome()
+
+    
+            
